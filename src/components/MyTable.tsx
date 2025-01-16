0b869704ac1a18e7e5f628862b024927b004fd07
eder <anonnimo21@yahoo.es>
2025-01-16 09:42:07 -0500

    Update MyTable.tsx

--- src/components/MyTable.tsx
+++ src/components/MyTable.tsx
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Card, Button } from '@mui/material';
 
 type ListadoSedesProps = {
     sedes: Sede[];
@@ -10,31 +10,34 @@ function ListadoSedes({ sedes, fetchSedes }: ListadoSedesProps) {
     fetchSedes();
 
     return (
-        <TableContainer component={Paper}>
+        <Card variant="outlined">
             <Typography variant="h4" align="center" gutterBottom>
                 Listado de Sedes
             </Typography>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Code</TableCell>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Foto</TableCell>
-                        <TableCell>CreationDate</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {sedes.map((sede) => (
-                        <TableRow key={sede.code}>
-                            <TableCell>{sede.code}</TableCell>
-                            <TableCell>{sede.name}</TableCell>
-                            <TableCell><img src={sede.foto} alt="Mi Imagen" width={100} /></TableCell>
-                            <TableCell>{sede.creationDate}</TableCell>
+            <Button variant="contained">Nueva Sede</Button>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Code</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Foto</TableCell>
+                            <TableCell>CreationDate</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {sedes.map((sede) => (
+                            <TableRow key={sede.code}>
+                                <TableCell>{sede.code}</TableCell>
+                                <TableCell>{sede.name}</TableCell>
+                                <TableCell><img src={sede.foto} alt="Mi Imagen" width={100} /></TableCell>
+                                <TableCell>{sede.creationDate}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Card>
     )
 
 }
